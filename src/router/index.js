63fb6fe45f7e5f3f3abad79d9df86b4530ea3f65
yanmengs <1657294032@qq.com>
2024-03-15 21:26:41 +0800

    feat-lly-init

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ import { useTokenStore } from '@/stores'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/login', component: () => import('@/views/login/LoginPage.vue') } // 登录页
+    { path: '/login', component: () => import('@/views/login/LoginPage.vue') }, // 登录页
     // {
     //   path: '/',
     //   component: () => import('@/views/layout/LayoutContainer.vue'),
